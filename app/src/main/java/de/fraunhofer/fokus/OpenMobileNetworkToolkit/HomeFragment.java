be4b5c18463d9f4c815609d4254e22f89fb9a19f
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2022-01-07 16:41:01 +0100

    initial commit

---
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/HomeFragment.java
@@ -0,0 +1,109 @@
+
+/*
+ * SPDX-FileCopyrightText: 2021 Peter Hasse <peter.hasse@fokus.fraunhofer.de>
+ * SPDX-FileCopyrightText: 2021 Fraunhofer FOKUS
+ *
+ * SPDX-License-Identifier: apache2
+ */
+
+package de.fraunhofer.fokus.OpenMobileNetworkToolkit;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.AccessNetworkConstants;
+import android.telephony.TelephonyManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.databinding.HomeFragmentBinding;
+
+public class HomeFragment extends Fragment {
+
+    private HomeFragmentBinding binding;
+    private boolean HasCarrierPrivilages;
+    private int sdk_version;
+
+    public void setHasCarrierPrivilages(boolean privilages) {
+        HasCarrierPrivilages = privilages;
+    }
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = HomeFragmentBinding.inflate(inflater, container, false);
+        sdk_version = Build.VERSION.SDK_INT;
+        return binding.getRoot();
+
+    }
+
+    //@SuppressLint("MissingPermission")
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        MainActivity ma = (MainActivity) getActivity();
+        setHasCarrierPrivilages(ma.HasCarrierPermissions());
+        TelephonyManager tm = ma.tm;
+        ArrayList<String> props = new ArrayList<String>();
+        if (HasCarrierPrivilages) {
+            props.add("Carrier Permissions: " + HasCarrierPrivilages);
+            props.add("Device Software Version: " + tm.getDeviceSoftwareVersion());
+            props.add("IMEI: " + tm.getImei());
+            props.add("SimSerial: " + tm.getSimSerialNumber());
+            props.add("SubscriberId: " + tm.getSubscriberId());
+            String tmp = "";
+            for (String plnm : tm.getForbiddenPlmns()) {
+                tmp += plnm + " ";
+            }
+            //java.util.List<String> blank = new List<String>();
+            tm.setForbiddenPlmns(new ArrayList<String>());
+            tm.setNetworkSelectionModeManual("00101", true, AccessNetworkConstants.AccessNetworkType.NGRAN);
+            props.add("Forbidden PLMS: " + tmp);
+            if (sdk_version >= Build.VERSION_CODES.R) {
+                props.add("SubscriptionId: " + tm.getSubscriptionId());
+            }
+            props.add("DataNetworkType: " + tm.getDataNetworkType());
+            props.add("DataState: " + tm.getDataState());
+            //java.util.List<android.telephony.CellInfo> cell_info = tm.getAllCellInfo();
+            if (ActivityCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                props.add("Cell Information: " + tm.getAllCellInfo());
+            }
+
+            props.add("SignalStrength: " + tm.getSignalStrength());
+            for (String prop : props) {
+                TextView tv = new TextView(getContext());
+                tv.setText(prop);
+                binding.mainInfos.addView(tv);
+            }
+        } else {
+            TextView tv = new TextView(getContext());
+            tv.setText("This app only works with Carrier Privilages. Make Sure you have the correct SHA1 fingerprint on your SIM Card.");
+            binding.mainInfos.addView(tv);
+        }
+        //binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        NavHostFragment.findNavController(HomeFragment.this)
+        //                .navigate(R.id.action_FirstFragment_to_SecondFragment);
+        //    }
+        //});
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
---
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/MainActivity.java
@@ -0,0 +1,153 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Peter Hasse <peter.hasse@fokus.fraunhofer.de>
+ * SPDX-FileCopyrightText: 2021 Fraunhofer FOKUS
+ *
+ * SPDX-License-Identifier: apache2
+ */
+
+package de.fraunhofer.fokus.OpenMobileNetworkToolkit;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.PersistableBundle;
+import android.telephony.CarrierConfigManager;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+    public CarrierConfigManager ccm;
+    private PersistableBundle cc;
+    public TelephonyManager tm;
+    public boolean cp = false;
+    private static final String TAG = "PhoenixCarrierConfig";
+
+    //@SuppressLint("MissingPermission")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //cc = ccm.getConfig();
+        tm = (TelephonyManager) getSystemService(this.TELEPHONY_SERVICE);
+        ccm = (CarrierConfigManager) getSystemService(this.CARRIER_CONFIG_SERVICE);
+        cp = HasCarrierPermissions();
+        // check permissions
+//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+//            Log.d(TAG, "Requesting READ_PHONE_STATE Permission");
+//            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_PHONE_STATE}, 1);
+//        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Requesting FINE_LOCATION Permission");
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 2);
+        }
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setSupportActionBar(binding.toolbar);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        binding.fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+        //FragmentManager fm = getSupportFragmentManager();
+        //HomeFragment f = (HomeFragment) fm.findFragmentById(R.id.home_fragment);
+
+        //f.setHasCarrierPrivilages(cp)
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.MediatekIMS) {
+            if (cp) {
+                tm.sendDialerSpecialCode("3646633");
+            } else {
+                NoCarrierToast();
+            }
+        } else if (id == R.id.SamsungIMS) {
+            if (cp) {
+                tm.sendDialerSpecialCode("467");
+            } else {
+                NoCarrierToast();
+            }
+        } else if (id == R.id.AndroidTesting) {
+            tm.sendDialerSpecialCode("4636");
+        } else if (id == R.id.SonyService) {
+            tm.sendDialerSpecialCode("7378423");
+        }else if (id == R.id.HuaweiProjektMenu) {
+            tm.sendDialerSpecialCode("2846579");
+        } else if (id == R.id.about) {
+            NavController navController = Navigation.findNavController(this, R.id.about_fragment);
+            navController.navigate(R.id.action_FirstFragment_to_SecondFragment);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int i, String[] strArr, int[] iArr) {
+        switch (i) {
+            case 1: {
+                if (iArr.length <= 0 || iArr[0] != 0) {
+                    Toast.makeText(this, "Could not get READ_PHONE_STATE permission ", Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(this, "Got READ_PHONE_STATE_PERMISSIONS", Toast.LENGTH_LONG).show();
+                }
+            }
+            case 2: {
+                if (iArr.length <= 0 || iArr[0] != 0) {
+                    Toast.makeText(this, "Could not get LOCATION permissions", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "Got LOCATION permissions", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    public void NoCarrierToast() {
+        Toast.makeText(this, "Carrier Permissions needed for this", Toast.LENGTH_LONG).show();
+    }
+
+    public boolean HasCarrierPermissions() {
+        return tm.hasCarrierPrivileges();
+    }
+
+}
