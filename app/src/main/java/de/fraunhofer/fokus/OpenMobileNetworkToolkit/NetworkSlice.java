654c193347ffd5c3995c9eb529c1797063ec1603
mohsinnisar <mohsin.nisar@fokus.fraunhofer.de>
2022-04-08 16:27:04 +0200

    NetworkSlice.java: Add slicing options

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/NetworkSlice.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/NetworkSlice.java
@@ -1,7 +1,16 @@
 package de.fraunhofer.fokus.OpenMobileNetworkToolkit;
 
+import static android.telephony.data.NetworkSliceInfo.SLICE_SERVICE_TYPE_EMBB;
+import static android.telephony.data.NetworkSliceInfo.SLICE_SERVICE_TYPE_MIOT;
 import static android.telephony.data.NetworkSliceInfo.SLICE_SERVICE_TYPE_URLLC;
+import static android.telephony.data.NetworkSliceInfo.SLICE_STATUS_CONFIGURED;
+import static android.telephony.data.NetworkSliceInfo.SLICE_STATUS_DEFAULT_CONFIGURED;
+import static android.telephony.data.NetworkSliceInfo.SLICE_STATUS_UNKNOWN;
 
+import android.app.slice.Slice;
+import android.app.slice.SliceItem;
+import android.app.slice.SliceManager;
+import android.app.slice.SliceSpec;
 import android.os.Build;
 import android.os.PersistableBundle;
 import android.service.carrier.CarrierIdentifier;
@@ -23,49 +32,66 @@ import java.util.Objects;
 @RequiresApi(api = Build.VERSION_CODES.S)
 public class NetworkSlice extends TelephonyManager {
     private static final String TAG = "NetworkSlicing";
-
+    private String dnn;
 
     public NetworkSlice(){
         super();
+        NetworkSliceInfo sliceURLLC = new NetworkSliceInfo.Builder()
+                .setSliceServiceType(SLICE_SERVICE_TYPE_URLLC)
+                .setStatus(SLICE_STATUS_DEFAULT_CONFIGURED)
+                .build();
+
+        NetworkSliceInfo sliceEMB = new NetworkSliceInfo.Builder()
+                .setSliceServiceType(SLICE_SERVICE_TYPE_EMBB)
+                .setStatus(SLICE_STATUS_CONFIGURED)
+                .build();
+
+        NetworkSliceInfo sliceMIOT = new NetworkSliceInfo.Builder()
+                .setSliceServiceType(SLICE_SERVICE_TYPE_MIOT)
+                .setStatus(SLICE_STATUS_UNKNOWN)
+                .build();
         Log.d(TAG, "Network Slicing");
     }
 
-    @Override
     public PersistableBundle onSliceConfig(CarrierIdentifier id){
         int sdk_version = Build.VERSION.SDK_INT;
         Log.i(TAG, "CarrierIdentifier id " + id.toString());
         PersistableBundle configForSlicing = new PersistableBundle();
 
-        NetworkSliceInfo sliceInfo = new NetworkSliceInfo.Builder()
-                .setSliceServiceType(SLICE_SERVICE_TYPE_URLLC)
-                .setStatus(NetworkSliceInfo.SLICE_STATUS_DEFAULT_CONFIGURED)
-                .build();
+        //Slice Manager
+        NetworkSlice
 
+
+
+        //Network Slice configuration
         NetworkSlicingConfig slicingConfig = new NetworkSlicingConfig();
         slicingConfig.getSliceInfo();
         slicingConfig.getUrspRules();
         slicingConfig.describeContents();
 
-        RouteSelectionDescriptor routeSelectionDescriptor = new RouteSelectionDescriptor();
+        //Route Selector
+        RouteSelectionDescriptor routeSelectionDescriptor = null;
+        assert false;
         routeSelectionDescriptor.getSliceInfo();
         routeSelectionDescriptor.getDataNetworkName();
         routeSelectionDescriptor.getSessionType();
         routeSelectionDescriptor.getPrecedence();
         routeSelectionDescriptor.getSscMode();
 
-
-
+        //Traffic Descriptor
         TrafficDescriptor trafficDescriptor = new TrafficDescriptor.Builder()
-                .setDataNetworkName("")
+                .setDataNetworkName(dnn)
                 .build();
+        trafficDescriptor.getDataNetworkName();
 
-        UrspRule urspRule = new UrspRule();
+        //URSP Rules
+        UrspRule urspRule = null;
+        assert false;
         urspRule.getRouteSelectionDescriptor();
         urspRule.getTrafficDescriptors();
-
 
         return null; //replace with network slice info
     }
 
-    
+
 }
