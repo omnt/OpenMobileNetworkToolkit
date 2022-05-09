9a85ad82f4d16d3ac13b5539499eb0d4a4cfb716
mohsinnisar <mohsin.nisar@fokus.fraunhofer.de>
2022-05-09 16:14:08 +0200

    OpenMobileNetworkToolkit.GB: Add global variables for later use

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/GlobalVars.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/GlobalVars.java
@@ -1,2 +1,7 @@
-package de.fraunhofer.fokus.OpenMobileNetworkToolkit;public class GlobalVars {
+package de.fraunhofer.fokus.OpenMobileNetworkToolkit;
+
+public class GlobalVars {
+        public static int counter = 0;
+        public static boolean isNetworkConnected = false;
+
 }
