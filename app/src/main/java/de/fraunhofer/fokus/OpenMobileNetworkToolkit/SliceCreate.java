d74ca717c9bbadb7435fe686edd03312fea2a4f1
mohsinnisar <mohsin.nisar@fokus.fraunhofer.de>
2022-08-09 15:00:12 +0200

    OpenMobileNetworkToolkit:SliceCreate: Add class for slice creation

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SliceCreate.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SliceCreate.java
@@ -1,2 +1,53 @@
-package de.fraunhofer.fokus.OpenMobileNetworkToolkit;public class SliceCreate {
+package de.fraunhofer.fokus.OpenMobileNetworkToolkit;
+
+import android.telephony.data.NetworkSliceInfo;
+
+public class SliceCreate {
+
+    public NetworkSliceInfo sliceCreate(int serviceType, int sliceDiff) {
+        NetworkSliceInfo sliceInfo = new NetworkSliceInfo.Builder()
+                .setSliceServiceType(serviceType)
+                .setSliceDifferentiator(sliceDiff)
+                .build();
+
+        return sliceInfo;
+    }
+
+    public NetworkSliceInfo sliceCreate(int serviceType, int sliceDiff, int status) {
+        NetworkSliceInfo sliceInfo = new NetworkSliceInfo.Builder()
+                .setSliceServiceType(serviceType)
+                .setSliceDifferentiator(sliceDiff)
+                .setStatus(status)
+                .build();
+
+        return sliceInfo;
+    }
+
+    public NetworkSliceInfo sliceCreate(int serviceType, int sliceDiff, int status, int mapped_hplmn_service_type, int mapped_hplmn_diff){
+        NetworkSliceInfo sliceInfo = new NetworkSliceInfo.Builder()
+                .setMappedHplmnSliceDifferentiator(mapped_hplmn_diff)
+                .setMappedHplmnSliceServiceType(mapped_hplmn_service_type)
+                .setSliceServiceType(serviceType)
+                .setSliceDifferentiator(sliceDiff)
+                .setStatus(status)
+                .build();
+
+        return sliceInfo;
+    }
+
+
+    public int getMappedHplmnSliceServiceType() {
+        return 0;
+    }
+
+    public int getMmappedHplmnSliceDifferentiator() {
+        return 0;
+    }
+
+    public void sliceCreateMapped(int serviceType, int sliceDiff) {
+        NetworkSliceInfo sliceInfo = new NetworkSliceInfo.Builder()
+                .setMappedHplmnSliceServiceType(serviceType)
+                .setMappedHplmnSliceDifferentiator(sliceDiff)
+                .build();
+    }
 }
