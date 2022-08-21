fe08f8e2892cdd156d0c1c53f161a53df6622d55
mohsinnisar <mohsin.nisar@fokus.fraunhofer.de>
2022-08-22 01:33:15 +0200

    OpenMobileNetworkToolkit: UI: fixed binding for UI

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/AboutFragment.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/AboutFragment.java
@@ -16,6 +16,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class AboutFragment extends Fragment
+
 {
     public AboutFragment() {
         super(R.layout.fragment_about);
