668c20f00eff95681ffd6f47e072153a2efd0a61
mohsinnisar <mohsin.nisar@fokus.fraunhofer.de>
2022-09-28 11:46:49 +0200

    OpenMobileNetworkToolkit: SettingPreference: Fix log preferene dependency, add switch case to loggerpreferences, implement preferenceChange for flagPreferences

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/FlagSettingFragment.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/FlagSettingFragment.java
@@ -1,15 +1,30 @@
 package de.fraunhofer.fokus.OpenMobileNetworkToolkit.SettingPreferences;
 
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceFragmentCompat;
 
-import de.fraunhofer.fokus.OpenMobileNetworkToolkit.R;
+import java.lang.annotation.Target;
 
-public class FlagSettingFragment extends PreferenceFragmentCompat {
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.R;
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.SRLog;
+
+public class FlagSettingFragment extends PreferenceFragmentCompat implements OnSharedPreferenceChangeListener {
+
+    public static String TAG = "version";
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preference_flag, rootKey);
+
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
     }
 }
--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/LoggingSettingsFragment.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/LoggingSettingsFragment.java
@@ -1,6 +1,7 @@
 package de.fraunhofer.fokus.OpenMobileNetworkToolkit.SettingPreferences;
 
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 
