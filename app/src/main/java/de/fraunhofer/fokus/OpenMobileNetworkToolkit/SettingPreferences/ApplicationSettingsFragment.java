c3a7adb0dd859255aa1f8f6e0c874739090088f4
mohsinnisar <mohsin.nisar@fokus.fraunhofer.de>
2022-09-28 11:13:48 +0200

    OpenMobileNetworkToolkit: SettingPreference: fix flicker for PreferenceChange and add logs

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/ApplicationSettingsFragment.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/ApplicationSettingsFragment.java
@@ -1,13 +1,30 @@
 package de.fraunhofer.fokus.OpenMobileNetworkToolkit.SettingPreferences;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.preference.PreferenceFragmentCompat;
 
 import de.fraunhofer.fokus.OpenMobileNetworkToolkit.R;
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.SRLog;
 
-public class ApplicationSettingsFragment extends PreferenceFragmentCompat{
+public class ApplicationSettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
+    public static String TAG = "PreferenceSettings";
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preference_application, rootKey);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        switch (s){
+            case "carrierPermission":
+                Boolean cp = sharedPreferences.getBoolean("carrierPermission", false);
+                SRLog.d(TAG,"Carrier Permission update: " + cp);
+                break;
+        }
     }
 }
--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/LoggingSettingsFragment.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/LoggingSettingsFragment.java
@@ -2,12 +2,18 @@ package de.fraunhofer.fokus.OpenMobileNetworkToolkit.SettingPreferences;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.google.android.material.tabs.TabLayout;
+
 import de.fraunhofer.fokus.OpenMobileNetworkToolkit.R;
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.SRLog;
 
 public class LoggingSettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
+    public static String TAG = "PreferenceSettings";
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preference_logging, rootKey);
@@ -18,6 +24,21 @@ public class LoggingSettingsFragment extends PreferenceFragmentCompat implements
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-        setPreferencesFromResource(R.xml.preference_logging, null);
+        switch (s){
+            case "enable_logging":
+                Boolean logger = sharedPreferences.getBoolean("enable_logging", false);
+                SRLog.d(TAG,"Logger update: " + logger);
+                break;
+            case "enable_notification_update":
+                Boolean notification = sharedPreferences.getBoolean("enable_notification_update", false);
+                SRLog.d(TAG,"Logger notification update: " + notification);
+                break;
+            case "enable_local_log":
+                Boolean local_log = sharedPreferences.getBoolean("enable_local_log", false);
+                SRLog.d(TAG,"Local log update: " + local_log);
+                break;
+
+        }
+        SRLog.d(TAG,"preference changed");
     }
 }
