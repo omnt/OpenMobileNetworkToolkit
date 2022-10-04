059cbfcaad5235db40e318a5c92e47570538e151
mohsinnisar <mohsin.nisar@fokus.fraunhofer.de>
2022-10-04 14:11:54 +0200

    OpenMobileNetworkToolkit: SettingPreference: Added callback on preference change for log preferences

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/LoggingSettingsFragment.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/LoggingSettingsFragment.java
@@ -4,8 +4,13 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -15,11 +20,19 @@ import de.fraunhofer.fokus.OpenMobileNetworkToolkit.SRLog;
 public class LoggingSettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
     public static String TAG = "PreferenceSettings";
 
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preference_logging, rootKey);
         getPreferenceScreen().getSharedPreferences()
                 .registerOnSharedPreferenceChangeListener(this);
+
+        SwitchPreferenceCompat enable_influx_switch = (SwitchPreferenceCompat) findPreference("enable_influx");
+        EditTextPreference influx_url_editText = (EditTextPreference) findPreference("influx_url");
+        EditTextPreference influx_org_editText = (EditTextPreference) findPreference("influx_org");
+        EditTextPreference influx_token_editText = (EditTextPreference) findPreference("influx_token");
+        EditTextPreference influx_bucket_editText = (EditTextPreference) findPreference("infoux_bucket");
+
     }
 
 
@@ -39,7 +52,26 @@ public class LoggingSettingsFragment extends PreferenceFragmentCompat implements
                 SRLog.d(TAG,"Local log update: " + local_log);
                 break;
 
+            case "enable_influx":
+                SRLog.d(TAG,"Enabled Influx");
+                break;
+
+            case "influx_url":
+                String influx_url = sharedPreferences.getString("influx_url","");
+                boolean flag = false;
+                if(influx_url == "") {
+                    SRLog.d(TAG,"URL not specified!");
+                    Toast.makeText(getContext(),"URL not specified!", Toast.LENGTH_SHORT).show();
+                    flag = false;
+                }
+                else {
+                    SRLog.d(TAG,"URL Found: " + influx_url);
+                    flag = true;
+                }
+                SRLog.d(TAG,"Influx URL found: "+flag);
+                break;
         }
+
         SRLog.d(TAG,"preference changed");
     }
 }
