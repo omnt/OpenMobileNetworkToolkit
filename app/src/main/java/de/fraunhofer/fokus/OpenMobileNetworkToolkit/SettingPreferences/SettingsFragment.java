2e2940a570fec3ba7cc349e1a01f62afc9519d86
mohsinnisar <mohsin.nisar@fokus.fraunhofer.de>
2022-10-05 12:53:06 +0200

    OpenMobileNetworkToolkit: SettingsFragment:Add recreate activity on destroy view to restart activity everytime we exit settings

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/SettingsFragment.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/SettingsFragment.java
@@ -1,10 +1,12 @@
 package de.fraunhofer.fokus.OpenMobileNetworkToolkit.SettingPreferences;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceHeaderFragmentCompat;
 
+import de.fraunhofer.fokus.OpenMobileNetworkToolkit.MainActivity;
 import de.fraunhofer.fokus.OpenMobileNetworkToolkit.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -12,5 +14,16 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preference, rootKey);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getActivity().recreate();
     }
 }
