07df7b33dd95fd57fa349077e143b3a076ed0321
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2023-06-15 17:31:08 +0200

    remove another srlog leftofver

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/FlagSettingFragment.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/SettingPreferences/FlagSettingFragment.java
@@ -2,16 +2,11 @@ package de.fraunhofer.fokus.OpenMobileNetworkToolkit.SettingPreferences;
 
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceFragmentCompat;
 
-import java.lang.annotation.Target;
-
 import de.fraunhofer.fokus.OpenMobileNetworkToolkit.R;
-import de.fraunhofer.fokus.OpenMobileNetworkToolkit.SRLog;
 
 public class FlagSettingFragment extends PreferenceFragmentCompat implements OnSharedPreferenceChangeListener {
 
