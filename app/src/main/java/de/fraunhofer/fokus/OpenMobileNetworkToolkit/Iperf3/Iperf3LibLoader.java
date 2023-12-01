1ef9a5b69c72bd8e7f4e0716b32d3007f418e900
Johann <johann.hackler@fokus.fraunhofer.de>
2023-12-01 12:59:49 +0100

    refactor iperf3LibLoader

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/Iperf3/Iperf3LibLoader.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/Iperf3/Iperf3LibLoader.java
@@ -1,6 +1,6 @@
 /*
  * SPDX-FileCopyrightText: 2023 Peter Hasse <peter.hasse@fokus.fraunhofer.de>
- *  SPDX-FileCopyrightText: 2023 Johann Hackler <johann.hackler@fokus.fraunhofer.de>
+ * SPDX-FileCopyrightText: 2023 Johann Hackler <johann.hackler@fokus.fraunhofer.de>
  * SPDX-FileCopyrightText: 2023 Fraunhofer FOKUS
  *
  * SPDX-License-Identifier: apache2
@@ -11,51 +11,43 @@ package de.fraunhofer.fokus.OpenMobileNetworkToolkit.Iperf3;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Iperf3LibLoader {
 
-
     private static boolean done = false;
     private static final String TAG = "Iperf3LibLoader";
+
+    private static final List<String> IPERF_LIBRARIES = List.of(
+        "iperf3.10.1",
+        "iperf3.11",
+        "iperf3.12",
+        "iperf3.15"
+    );
 
     protected static synchronized void load() {
         if (done) {
             return;
         }
-        ArrayList<Integer> loadedLibs = new ArrayList<>();
-        try {
-            System.loadLibrary("iperf3.10.1");
-            Log.i(TAG, "iperf3.10.1 loaded!");
-            loadedLibs.add(1);
-        } catch (UnsatisfiedLinkError ignored) {
-        }
 
-        try {
-            System.loadLibrary("iperf3.11");
-            Log.i(TAG, "iperf3.11 loaded!");
-            loadedLibs.add(1);
-        } catch (UnsatisfiedLinkError ignored) {
+        List<String> loadedLibs = new ArrayList<>();
 
-        }
-
-        try {
-            System.loadLibrary("iperf3.12");
-            Log.i(TAG, "iperf3.12 loaded!");
-            loadedLibs.add(1);
-        } catch (UnsatisfiedLinkError ignored) {
-            return;
-        }
-        try {
-            System.loadLibrary("iperf3.15");
-            Log.i(TAG, "iperf3.15 loaded!");
-            loadedLibs.add(1);
-        } catch (UnsatisfiedLinkError ignored) {
+        for (String library : IPERF_LIBRARIES) {
+            try {
+                System.loadLibrary(library);
+                Log.i(TAG, library + " loaded!");
+                loadedLibs.add(library);
+                done = true;
+                return;
+            } catch (UnsatisfiedLinkError ignored) {
+            }
         }
 
         if (loadedLibs.size() > 1) {
             Log.d(TAG, "static initializer: multiple libiperfs loaded!");
+        } else if (loadedLibs.isEmpty()) {
+            Log.d(TAG, "failed to load any iPerf3 libs!");
         }
         done = true;
     }
-
 }
