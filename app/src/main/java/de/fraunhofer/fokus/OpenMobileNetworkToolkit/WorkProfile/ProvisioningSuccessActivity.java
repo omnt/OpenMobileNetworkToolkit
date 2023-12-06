bc8a49ae190b1f3be118a5b95f558c6967f27265
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2023-12-06 15:00:40 +0100

    add mohsin back to the spdx headers

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/WorkProfile/ProvisioningSuccessActivity.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/WorkProfile/ProvisioningSuccessActivity.java
@@ -1,6 +1,7 @@
 /*
  * SPDX-FileCopyrightText: 2023 Peter Hasse <peter.hasse@fokus.fraunhofer.de>
  *  SPDX-FileCopyrightText: 2023 Johann Hackler <johann.hackler@fokus.fraunhofer.de>
+ * SPDX-FileCopyrightText: 2023 Mohsin Nisar
  * SPDX-FileCopyrightText: 2023 Fraunhofer FOKUS
  *
  * SPDX-License-Identifier: apache2
