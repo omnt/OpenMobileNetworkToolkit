832f6f3271e86fcad9eb310e1aa03178c25f4fed
Peter Hasse <peter.hasse@fokus.fraunhofer.de>
2024-10-22 09:30:29 +0200

    reworked duplicate logging strings

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/LoggingService.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/LoggingService.java
@@ -353,7 +353,7 @@ public class LoggingService extends Service {
                     p.time(time, WritePrecision.MS);
                     p.addTags(tags_map);
                 } else {
-                    Log.w(TAG, "Point without fields getCellInfoPoint");
+                    Log.w(TAG, "Point without fields from getCellInformationPoint");
                 }
             }
             logPoints.addAll(ps);
@@ -366,7 +366,7 @@ public class LoggingService extends Service {
                     p.time(time, WritePrecision.MS);
                     p.addTags(tags_map);
                 } else {
-                    Log.w(TAG, "Point without fields getCellInfoPoint");
+                    Log.w(TAG, "Point without fields from getNetworkInformationPoints");
                 }
             }
             logPoints.addAll(ps);
