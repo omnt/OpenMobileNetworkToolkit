2beea70dcb557443f31176d4f3e44040b3e4a7eb
RkShaRkz <darksidemetatron@gmail.com>
2024-11-04 12:15:33 +0100

    Added nullguarding to QuickFragment too

--- app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/QuickFragment.java
+++ app/src/main/java/de/fraunhofer/fokus/OpenMobileNetworkToolkit/QuickFragment.java
@@ -1,5 +1,6 @@
 package de.fraunhofer.fokus.OpenMobileNetworkToolkit;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
@@ -305,31 +307,36 @@ public class QuickFragment extends Fragment {
      final Runnable updateUI = new Runnable() {
         @Override
         public void run() {
-            mainLL.removeAllViews();
-            List<CellInformation> cellInformationList = dp.getRegisteredCells();
-            List<CellInformation> neighborCells = dp.getNeighbourCellInformation();
-            if(cellInformationList.isEmpty()){
-                LinearLayout error = new LinearLayout(context);
-                error.setLayoutParams(new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                ));
-                error.setOrientation(LinearLayout.VERTICAL);
-                TextView errorText = new TextView(context);
-                errorText.setLayoutParams(new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                ));
-                errorText.setText(getString(R.string.cell_na));
-                error.addView(errorText);
-                mainLL.addView(error);
-            } else {
-                cellInformationList.forEach(cellInformation -> addCellInformationToView(cellInformation));
-            }
-            if (spg.getSharedPreference(SPType.default_sp).getBoolean("show_neighbour_cells", false)) {
-                if(!neighborCells.isEmpty()){
-                    neighborCells.forEach(cellInformation -> addCellInformationToView(cellInformation));
+            Activity parentActivity = getActivity();
+            if (parentActivity != null && isAdded()) {
+                mainLL.removeAllViews();
+                List<CellInformation> cellInformationList = dp.getRegisteredCells();
+                List<CellInformation> neighborCells = dp.getNeighbourCellInformation();
+                if (cellInformationList.isEmpty()) {
+                    LinearLayout error = new LinearLayout(context);
+                    error.setLayoutParams(new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                    ));
+                    error.setOrientation(LinearLayout.VERTICAL);
+                    TextView errorText = new TextView(context);
+                    errorText.setLayoutParams(new LinearLayout.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                    ));
+                    errorText.setText(getString(R.string.cell_na));
+                    error.addView(errorText);
+                    mainLL.addView(error);
+                } else {
+                    cellInformationList.forEach(cellInformation -> addCellInformationToView(cellInformation));
                 }
+                if (spg.getSharedPreference(SPType.default_sp).getBoolean("show_neighbour_cells", false)) {
+                    if (!neighborCells.isEmpty()) {
+                        neighborCells.forEach(cellInformation -> addCellInformationToView(cellInformation));
+                    }
+                }
+            } else {
+                Toast.makeText(getContext(), "Tried updating QuickFragment UI while not being added/attached!", Toast.LENGTH_SHORT).show();
             }
             updateUIHandler.postDelayed(updateUI, 500);
         }
